b16dee3ce9ccf8839afae81c7b506a7983a89893
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-07 01:05:17 +0530

    part1 complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,14 +1,62 @@
 import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
 export const sendMessage = async (req, res) => {
     try {
         const { message } = req.body;
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
+        
+        let conversation = await Conversation.findOne({
+            participants : { $all: [senderId, receiverId]},
+        })
 
-        await C
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            })
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+
+        // SOCKET.IO FUNCTIONALITY WILL GO HERE
+        
+        // await conversation.save();
+        // await newMessage.save();
+
+
+        //this will run in parallel
+        await Promise.all([conversation.save(), newMessage.save()])
+
+        res.status(201).json(newMessage)
     } catch (error) {
         console.log("error in sendMessage controller: ", error.message);
         res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getMessage = async (req,res) => {
+    try {
+        const {id: userToChatId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId, userToChatId]}
+        }).populate('messages'); //populate will get the message object from the message collection(Not reference but actual message)
+
+        if(!conversation) return res.status(200).json([])
+
+        const message = conversation.messages;
+        res.status(200).json(message)
+    } catch (error) {
+        console.log("Error in getMessage controller:", error.message)
+        res.status(500).json({error:"Internal server error"})
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
 import connectToMongodb from './db/connectToMongodb.js'
 
 const app = express();
@@ -11,17 +13,18 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(express.json()); // Middleware to parse JSON request bodies(to parse the incoming requests with JSON payloads (from req.body))
 app.use(cookieParser()); // Middleware to parse cookies
 
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes)
 
 
-app.get('/',(req,res) => {
-    res.send("Hello world!!");
-})
+// app.get('/',(req,res) => {
+//     res.send("Hello world!!");
+// })
 
 app.use('/api/auth',authRoutes)
 
