ee171f4fb8bd2abc4d63351c06b08cba04740099
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-07 23:03:55 +0530

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="p-4 h-screen flex items-center justify-center">
+    {/* <Login /> */}
+    <SignUp />
+    </div>
+    </>
+  )
+}
+
+export default App
