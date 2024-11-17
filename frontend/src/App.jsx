0c13bdbb7e3e0c918e1df6ed4105a70ddcc2ddb2
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-17 23:38:40 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from './pages/signup/SignUp'
 import './App.css'
 
 import Home from './pages/home/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -11,9 +12,11 @@ function App() {
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
-    {/* <Login /> */}
-    {/* <SignUp /> */}
-    <Home />
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
     </div>
     </>
   )
