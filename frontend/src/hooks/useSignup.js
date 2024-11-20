a52794687d48dd5c46119a4ac1a1ad493023fc2b
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-20 23:49:45 +0530

    commit

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false);
-
+    const { setAuthUser} = useAuthContext();
     const signup = async ({fullName,username,password, confirmPassword, gender}) => {
         const success = handleInputErrors({fullName,username,password, confirmPassword, gender});
         if(!success) return;
@@ -23,7 +24,9 @@ const useSignup = () => {
             }
             // console.log(data);
             // localstorage
+            localStorage.setItem("chat-user", JSON.stringify(data));
             // context
+            setAuthUser(data);
         }
         catch {
             toast.error(error.message)
