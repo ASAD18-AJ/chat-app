b6916cac60831f67b90fef9f74fcf6a6f80e8f4f
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-21 23:53:14 +0530

    update

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -94,7 +94,7 @@ const SignUp = () => {
 
           <div>
             <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+              {loading ? <span className="loading loading-spinner"></span>:"sign Up"}
             </button>
           </div>
         </form>
