cb4fc2dd0e88d24519105f38d80fa628081b7d41
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-28 23:29:03 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -7,13 +8,15 @@ import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 import connectToMongodb from './db/connectToMongodb.js'
+import { app, server } from "./socket/socket.js";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 dotenv.config();
 
 const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Middleware to parse JSON request bodies(to parse the incoming requests with JSON payloads (from req.body))
 app.use(cookieParser()); // Middleware to parse cookies
@@ -35,7 +38,7 @@ app.get("*", (req, res) => {
 
 app.use('/api/auth',authRoutes)
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongodb()
     console.log(`Server Running on Port ${PORT}`)
 })  
