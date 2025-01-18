5097f196ed85106517c20b24b49e92aaaea8f47f
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2025-01-18 15:22:15 +0530

    commit

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -4,20 +4,20 @@ import toast from "react-hot-toast";
 
 const useSendMessage = () => {
     const [loading, setLoading] = useState(false)
-    const {messages, setMessages, selectedConversation } = useConversation();
+    const { messages, setMessages, selectedConversation } = useConversation();
 
     const sendMessage = async (message) => {
         setLoading(true);
         try {
-            const res = await fetch(`/api/messages/send/${selectedConversation._id}`,{
+            const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({message})
+                body: JSON.stringify({ message })
             })
             const data = await res.json();
-            if(data.error) throw new Error(data.error);
+            if (data.error) throw new Error(data.error);
             setMessages([...messages, data.message])
         } catch (error) {
             toast.error(error.message)
